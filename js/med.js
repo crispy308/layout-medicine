6692bcfeea685a6eb6a4a75485e0828d8c2cdea6
crispy308 <boltunovv6@mail.ru>
2021-03-03 21:20:58 +0400

    add new layout medicine

---
+++ js/med.js
@@ -0,0 +1,13 @@
+$(function () {
+    $('.lays__inner').slick({
+        infinite: true,
+        slidesToShow: 1, 
+        slidesToScroll: 1,
+        speed: 1000,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        dots: true,
+        arrows: true
+    })
+})
+
